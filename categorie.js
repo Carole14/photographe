1cb5f82caca813642ef2c718c3f7e8992d606350
Carole Belle <b.carole14@gmail.com>
2022-04-05 11:10:53 +0200

    ajout

---
+++ categorie.js
@@ -0,0 +1,8 @@
+let categories: [
+    {categorie : mariage,},
+    {categorie : bapteme,},
+    {categorie : bébé,},
+    {categorie : couple,},
+    {categorie : grossesse,},
+    {categorie : famille},
+]
