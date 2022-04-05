0bbbaf339bce98df93a6471048d2a3505cbb5e36
Carole Belle <b.carole14@gmail.com>
2022-04-05 11:25:43 +0200

    modification orthographe

--- categorie.js
+++
@@ -1,8 +0,0 @@
-let categories: [
-    {categorie : mariage,},
-    {categorie : bapteme,},
-    {categorie : bébé,},
-    {categorie : couple,},
-    {categorie : grossesse,},
-    {categorie : famille},
-]
